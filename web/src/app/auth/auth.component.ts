8674a0bfb005375fc814290c54a56c74eac895ae
Syafiq Basri <altsyafiq@gmail.com>
2023-12-19 22:40:58 +0800

    feat: improve responsizeness

--- web/src/app/auth/auth.component.ts
+++ web/src/app/auth/auth.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 			class="flex flex-col justify-center align-middle p-10
 			h-screen bg-zinc-900 text-zinc-200">
 			<div class="grid grid-cols-12">
-				<div class="col-start-5 col-span-3">
+				<div class="col-span-12 lg:col-start-5 lg:col-span-3">
 					<router-outlet></router-outlet>
 				</div>
 			</div>
