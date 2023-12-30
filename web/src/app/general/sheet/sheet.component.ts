6160e701a91cc01f859c5bfd6d9581955bb862b2
Syafiq Basri <altsyafiq@gmail.com>
2023-12-30 17:51:50 +0800

    feat: added initial sheet component design

---
+++ web/src/app/general/sheet/sheet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sheet',
+  templateUrl: './sheet.component.html',
+  styles: [
+  ]
+})
+export class SheetComponent {
+
+}
