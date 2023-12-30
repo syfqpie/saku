627c798a6bc7869861744b3d1faf8c0b6a8c8157
Syafiq Basri <altsyafiq@gmail.com>
2023-12-30 18:15:03 +0800

    feat: added imports

--- web/src/app/general/general.module.ts
+++ web/src/app/general/general.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
-import { HomeComponent } from './home/home.component';
 import { GeneralComponent } from './general.component';
 import { GeneralRoutes } from './general.routing';
+import { HomeComponent } from './home/home.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
 import { MenuSidebarComponent } from './components/menu-sidebar/menu-sidebar.component';
+import { SheetComponent } from './sheet/sheet.component';
 import { UserSidebarComponent } from './components/user-sidebar/user-sidebar.component';
 import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
 
@@ -13,7 +15,9 @@ import { UserAvatarComponent } from './components/user-avatar/user-avatar.compon
 	declarations: [
 		GeneralComponent,
 		HomeComponent,
+		ItemListComponent,
 		MenuSidebarComponent,
+		SheetComponent,
 		UserSidebarComponent,
 		UserAvatarComponent,
 	],
