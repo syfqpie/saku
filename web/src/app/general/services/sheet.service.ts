2b2257f699b2aa0f87d59e4dbd6da52715bbdc7d
Syafiq Basri <altsyafiq@gmail.com>
2023-12-31 18:05:08 +0800

    feat: added SheetService

---
+++ web/src/app/general/services/sheet.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { API_VERSION, SHEETS_PREFIX } from 'src/app/shared/constants/prefixes.constant';
+import { Sheet } from '../models/sheets.model';
+
+const BASE_URL = `${environment.baseUrl}${API_VERSION}/${SHEETS_PREFIX}/`
+
+
+@Injectable({
+	providedIn: 'root'
+})
+export class SheetService {
+	private sheetsSubject: BehaviorSubject<Sheet[]> = new BehaviorSubject<Sheet[]>([])
+	public sheets: Observable<Sheet[]>
+
+	constructor(
+		private http: HttpClient
+	) {
+		this.sheets = this.sheetsSubject.asObservable();
+	}
+
+	/**
+	 * List owned sheets
+	 * 
+	 * @returns list of sheet objects
+	 */
+	public listSheets(): Observable<Sheet[]> {
+		const urlTemp = `${BASE_URL}`
+		return this.http.get<Sheet[]>(urlTemp)
+			.pipe(map((sheets) => {
+				this.sheetsSubject.next(sheets)
+				return sheets
+			}))
+	}
+
+	/**
+	 * Get sheet with id
+	 * 
+	 * @param id id of sheet
+	 * @returns sheet object
+	 */
+	public getSheet(id: string): Observable<Sheet> {
+		const urlTemp = `${BASE_URL}${id}`
+		return this.http.get<Sheet>(urlTemp)
+			.pipe(map((sheet) => {
+				return sheet
+			}))
+	}
+
+}
--- web/src/app/shared/constants/prefixes.constant.ts
+++ web/src/app/shared/constants/prefixes.constant.ts
@@ -1,3 +1,4 @@
 export const AUTH_PREFIX = 'auth'
 export const API_VERSION = 'v1'
 export const PROFILES_PREFIX = 'profiles'
+export const SHEETS_PREFIX = 'sheets'
