27fecfa8c9ad66e01e8f1a13fd0fdc8db97e9982
Syafiq Basri <altsyafiq@gmail.com>
2024-01-02 20:18:21 +0800

    feat: removed unused key

--- web/src/app/general/models/sheets.model.ts
+++ web/src/app/general/models/sheets.model.ts
@@ -1,10 +1,7 @@
 export interface SheetItem {
-    id: string;
     item: string;
     amount: number;
     is_checked: boolean;
-    created_at?: string;
-    modified_at?: string;
 }
 
 export interface Sheet {
