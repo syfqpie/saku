8c224c4277988fcfeb90b05741ac5f8feb4ebf6c
Syafiq Basri <altsyafiq@gmail.com>
2024-01-02 12:57:09 +0800

    feat: improved item list component by adding sheet item form

--- web/src/app/general/components/item-list/item-list.component.ts
+++ web/src/app/general/components/item-list/item-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { SheetItem } from '../../models/sheet.model';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SheetItem } from '../../models/sheets.model';
 
 @Component({
 	selector: 'app-item-list',
@@ -9,8 +10,44 @@ import { SheetItem } from '../../models/sheet.model';
 export class ItemListComponent {
 	@Input()
 		items: SheetItem[] = []
+	
+	form: FormGroup = this.fb.group({
+		items: this.fb.array([])
+	})
+
+	constructor(
+		private fb: FormBuilder
+	) {}
+
+	get $items() {
+		return this.form.get('items') as FormArray
+	}
 
 	get totalValue() {
-		return this.items.reduce((acc, array) => acc + array.value, 0)
+		return this.$items.value.reduce((acc: number, array: SheetItem) => acc + array.amount, 0)
 	}
+
+	public generateRow(
+		item: string | null = null,
+		amount: number | null = null,
+		is_checked = false
+	) {
+		return this.fb.group({
+			item: [item, Validators.required],
+			amount: [amount, Validators.compose([
+				Validators.required,
+				Validators.min(0)
+			])],
+			is_checked: [is_checked, Validators.required]
+		})
+	}
+
+	public addItem() {
+		this.$items.push(this.generateRow())
+	}
+
+	public deleteItem(index: number) {
+		this.$items.removeAt(index)
+	}
+
 }
