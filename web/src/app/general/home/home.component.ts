797401d33044e035c101e82e23adc6229cdd9294
Syafiq Basri <altsyafiq@gmail.com>
2024-01-07 21:22:31 +0800

    feat: designed home page

--- web/src/app/general/home/home.component.ts
+++ web/src/app/general/home/home.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription, first } from 'rxjs';
+
+import { Sheet } from '../models/sheets.model';
+import { SheetService } from '../services/sheet.service';
+import { LoadableComponent } from 'src/app/shared/models/base.model';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
-	styles: []
+	styles: [],
 })
-export class HomeComponent {
+export class HomeComponent extends LoadableComponent implements OnInit, OnDestroy {
 
+	public sheets: Sheet[] = []
+	private subscription = new Subscription
+
+	constructor(
+		private router: Router,
+		private sheetSvc: SheetService
+	) {
+		super()
+	}
+
+	ngOnInit(): void {
+		this.getData()
+	}
+
+	ngOnDestroy(): void {
+		if (this.subscription) this.subscription.unsubscribe()
+	}
+
+	public getData() {
+		this.isLoading = true
+		this.subscription.add(this.sheetSvc.listSheets()
+			.pipe(first())
+			.subscribe({
+				next: (sheets) => {
+					this.sheets = sheets
+					this.isLoading = false
+				},
+				error: (_error) => {
+					this.isLoading = false
+				},
+			}))
+	}
+
+	public addNew() {
+		return this.router.navigate(['/sheet', 'new'])
+	}
 }
